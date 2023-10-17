cb2e1b93286c25281432f8d05b64d2afae66d62c
dongdong <2434205538@qq.com>
2023-10-17 13:55:05 +0800

    .

---
+++ client2.js
@@ -0,0 +1,19 @@
+
+if(bar){
+    window.reload(bar)
+}
+else {
+    window.reload=(bar)=>{
+        history.go(0)
+    }
+    var bar
+}
+window.onDouyinServer = function() {
+    bar=new Barrage()
+}
+console.clear()
+console.log(`[${new Date().toLocaleTimeString()}]`, '正在载入JS,请稍后..')
+console.log(`[${new Date().toLocaleTimeString()}]`, '如需删除直播画面，请在控制台输入: removeVideoLayer()')
+var scriptElement = document.createElement('script')
+scriptElement.src = 'https://yycdongdong.github.io/index.js?t=' + Math.random()
+document.body.appendChild(scriptElement)
